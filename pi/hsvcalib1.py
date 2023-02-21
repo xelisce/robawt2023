f782e9344734a064dfe12030be3264e58502dfc3
xelisce <xelisce@gmail.com>
2023-02-21 23:42:41 +0800

    fixed hsvcalib

--- pi/hsvcalib1.py
+++ pi/hsvcalib1.py
@@ -4,6 +4,7 @@ from MultiThread import WebcamStream
 import cv2
 
 cam_stream = WebcamStream(stream_id=0)
-cam_stream.start(0)
+cam_stream.start()
 frame = cam_stream.read()
 cv2.imwrite("hsvcalibimage.jpg", frame)
+cam_stream.stop()
