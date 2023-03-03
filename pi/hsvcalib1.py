6ea6637afe218c18c68312e34d971dce94c65daa
Mice <nicemiceinice@gmail.com>
2023-03-03 17:33:07 +0800

    updated hsvcalib1.py :skull:

--- pi/hsvcalib1.py
+++ pi/hsvcalib1.py
@@ -6,14 +6,15 @@ import cv2
 cam_stream = WebcamStream(stream_id=0)
 cam_stream.start()
 
-key = cv2.waitKey(1)
+
 while True:
     if cam_stream.stopped == True:
         break
     else:
         frame = cam_stream.read()
-    
+
     cv2.imshow('frame', frame)
+    key = cv2.waitKey(1)
     if key == ord('k'): #press 'k' to take a photo 
         cv2.imwrite("hsvcalibimage.jpg", frame)
     elif key == ord('q'): #press q to quit
