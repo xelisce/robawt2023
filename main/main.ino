a6e5b573b1caedfa7994de6d51ca8bf0051e3ff7
Mice <nicemiceinice@gmail.com>
2023-03-15 22:27:19 +0800

    added merged linetrack + updated main.ino

--- main/main.ino
+++ main/main.ino
@@ -16,9 +16,9 @@
 
 //* OBJECT INITIALISATIONS */
 Motor MotorR(13, 12, 19, 18); //M2 swapped
-Motor MotorL(10, 11, 0, 1); //M1
+Motor MotorL(10, 11, 1, 0); //M1
 Vroom Robawt(&MotorL, &MotorR);
-VL53L0X l_tof, r_tof;
+VL53L0X front_tof; //, r_tof;
 Servo servos[2];
 
 //* SERVOS CONSTANTS SETUP */
@@ -44,11 +44,11 @@ const int TX1PIN = 8,
   RX0PIN = 17,
   SDAPIN = 4,
   SCLPIN = 5,
-  SWTPIN = 28;
-
+  SWTPIN = 28,
+  LEDPIN = 3;
 
 int serialState = 0;
-int l_dist = 0;
+int front_dist = 0;
 
 double rotation = 0;
 double rpm = 40;
@@ -65,20 +65,25 @@ double prev_L_dist = 0;
 double prev_R_dist = 0;
 double start_L_dist = 0;
 double start_R_dist = 0;
+double rev_L_dist = 0;
+double rev_R_dist = 0;
+double prev_rotation = 0;
 
 long startBlueMillis = 0;
 long timeElapsed = 0;
 long startReverse = 0;
 
+elapsedMillis sinceStop;
 
 void setup() {
 
   pinMode(SWTPIN, INPUT_PULLDOWN);
+  pinMode(LEDPIN, OUTPUT);
 
   //* SERVOS */
   for (int i = Servos::LEFT; i != (Servos::RIGHT + 1); i++) {
     servos[i].writeMicroseconds(pwmangle(servos_angle[i], servos_max_angle[i]));
-    servos[i].attach(servos_pin[i], 500, 2500);
+    servos[i].attach(servos_pin[i], 500, 2500); // (pin, min, max)
   }
 
   //* USB SERIAL COMMS */
@@ -96,8 +101,8 @@ void setup() {
   //* MULTIPLEXER */
   businit(&Wire, SDAPIN, SCLPIN);
 
-//  l0xinit(&Wire, &l_tof, 3);
-  // l0xinit(r_tof, 5);
+  l0xinit(&Wire, &front_tof, 4);
+  //l0xinit(r_tof, 5);
 
   //* MOTOR ENCODERS */    //^ basically interrupts the main code to run the encoder code
   attachInterrupt(MotorL.getEncAPin(), ISRLA, RISING);
@@ -106,33 +111,31 @@ void setup() {
   attachInterrupt(MotorR.getEncBPin(), ISRRB, RISING);
 }
 
+//* -----------END SETUP-----------*//
+
 
 void loop() {
+  
+  digitalWrite(LEDPIN, HIGH); // Turns on LED by setting it to high voltage?
 
   if (servos_change) {
     for (int i = Servos::LEFT; i != (Servos::RIGHT + 1); i++) {
       servos[i].writeMicroseconds(pwmangle(servos_angle[i], servos_max_angle[i]));
     }
+    servos_change = false; //^ DOM: not sure if this is correct
   }
-//
-//  Serial.print("Distance (cm): ");
-//  Serial.println(MotorL.getDist());
-//  Serial.print("Angle (deg): ");
-//  Serial.println(MotorL.getAngle());
-//  Serial.print("Enc Val: ");
-//  Serial.println(MotorL.getEncVal());
 
-  serialEvent();
+  serialEvent();  //^ DOM: Reminder for anyone reading this to uncomment this when testing LOL
 
   if (!digitalRead(SWTPIN)) {
 
-    claw_open();
+    //claw_open();
 
     //* HANDLING THE INFO RECEIVED */
     switch (task)
     {
 
-      //~ Handling the continue turning after green squares
+      //~ Handling the continue turning after green squares 
       case 0:
         if (curr == 1) {
           curr = 10;
@@ -140,7 +143,7 @@ void loop() {
         } else if (curr == 2) {
           curr = 11;
           lostGSMillis = millis();
-        } else if (curr != 10 || curr != 11) {
+        } else if (curr != 10 && curr != 11 && curr != 7) {       //^ DOM: I'll add a temp case for rescue kit :thumbs: 
           curr = 0;
         } 
         break;
@@ -174,47 +177,77 @@ void loop() {
         Robawt.setSteer(0, 0);
         Robawt.reset();
         break;
-//
-//      case 5: //turning towards blue
-//        Robawt.setSteer(rpm, rotation);
-//        if (start_L_dist == 0) {start_L_dist = MotorL.getDist(); start_R_dist = MotorR.getDist();}
+
+      case 5: //turning towards blue
+        Robawt.setSteer(rpm, rotation);
+        if (prev_rotation == 0) prev_rotation = rotation;
+        if (start_L_dist == 0) {start_L_dist = MotorL.getDist(); start_R_dist = MotorR.getDist();}
+        prev_L_dist = MotorL.getDist();
+        prev_R_dist = MotorR.getDist();
+        //* Debug motor enc values */
 //        Serial.print("Left Motor: ");
 //        Serial.println(MotorL.getDist());
 //        Serial.print("Right Motor: ");
 //        Serial.println(MotorR.getDist());
-//        prev_L_dist = MotorL.getDist();
-//        prev_R_dist = MotorR.getDist();
-//        break;
-//      case 6: //relatively centred on blue? TEMPORARY
-//        Robawt.setSteer(rpm , 0);
-//        if (startBlueMillis == 0) {startBlueMillis = millis();}
-//        //? DOM: insert code for picking up the block? not sure how that'll wrk
-//        //? Or should I handle the reversal of the robot in a separate case instead
-//        //? XEL: yes pls diff case
-//        /*
-//        if (servos_change){
-//          include timeelapsed variable
-//          if (timeElapsed == 0) {timeElapsed = millis() - startBlueMillis; double startReverse = millis()
-//          if (millis - startReverse < timeElapsed) Robawt.setSteer(-rpm, 0);
-//          else if ((MotorL.getDist() - prev_L_dist <= prev_L_dist - start_L_dist) &&  motorR.getDist() - prev_R_dist <= prev_R_dist - start_R_dist)
-//          {
-//            Robawt.setSteer(-rpm, rotation)
-//          }
-//          else 
-//          }
-//
-//        */
-//        break;
-//      case 7: //^ reversing; FOR TESTING PURPOSES ONLY!!! Remove afterwards.
-//        if (timeElapsed == 0) {timeElapsed = millis() - startBlueMillis; startReverse = millis();}
-//        if (millis() - startReverse < timeElapsed) Robawt.setSteer(-rpm, 0);
-//        else if ((MotorL.getDist() - prev_L_dist <= prev_L_dist - start_L_dist) &&  (MotorR.getDist() - prev_R_dist <= prev_R_dist - start_R_dist))
-//        {
-//          Robawt.setSteer(-rpm, rotation);
-//        }
-//        else Robawt.setSteer(0,0);
-//        break;
-//
+        break;
+
+      case 6: //relatively centred on blue
+        Robawt.setSteer(rpm , 0);
+        if (startBlueMillis == 0) {startBlueMillis = millis();}
+        //TODO: insert code for picking up the block? not sure how that'll work
+        /*pseudocode:
+        if (front_dist < 30) {
+            stop for 1s? 
+            pick up 
+            if (servos_change) curr = 7;
+        }
+        */
+       break;
+       
+      case 7: //Reversing bot (reminder to make it pico side only)
+        if (timeElapsed == 0) {
+          timeElapsed = millis() - startBlueMillis; 
+          startReverse = millis(); 
+          sinceStop = 0; //temporary; used to stop the bot
+        }
+        
+        if (sinceStop < 2000) Robawt.setSteer(0, 0); //^ DOM: Remove this debug shit later
+        else { 
+        //* Debug varibles */
+//        Serial.print("ABS value");
+//        Serial.println(abs(MotorL.getDist() - rev_L_dist));
+//        Serial.print("prev - start");
+//        Serial.println(prev_L_dist - start_L_dist);
+
+        //~ Move backwards first
+        if (millis() - startReverse <= timeElapsed) {
+          Robawt.setSteer(-rpm, 0);
+        }
+        
+        //~ Rotating in the opposite direction
+        else { 
+          if (rev_L_dist == 0) {rev_L_dist = MotorL.getDist(); rev_R_dist = MotorR.getDist(); }
+          double L_dist_to_travel = prev_L_dist - start_L_dist;
+          double R_dist_to_travel = prev_R_dist - start_R_dist;
+          double dist_L_Travelled = abs(MotorL.getDist() - rev_L_dist); //^ ABSing cuz distance will decrease when moving backwards
+          double dist_R_Travelled = abs(MotorR.getDist() - rev_R_dist);
+
+          //^ If motor hasn't travelled the requisite distance
+          if ((dist_L_Travelled <= L_dist_to_travel) && (dist_R_Travelled <= R_dist_to_travel)) {
+            Robawt.setSteer(-rpm, prev_rotation);
+          }
+          else {
+            //curr = 0;
+            Robawt.setSteer(0, 0);
+            Robawt.reset();
+//            Serial.print("Finished reversing");
+            //^ DOM: reset variables to test blue lolz
+            //timeElapsed = 0, startBlueMillis = 0, prev_rotation = 0, start_L_dist = 0, rev_L_dist = 0;
+          }
+        }
+        }
+        break;
+
       case 10: //only pico side --> just lost left green, return to lt
         if (millis() - lostGSMillis > 100) curr = 0;
         else Robawt.setSteer(rpm, -0.5);
@@ -225,18 +258,14 @@ void loop() {
 
     }
 
-    //* READ LIDAR */
-    // tcaselect(3);
-    // l_dist = l_tof.readRangeContinuousMillimeters();
-    // Serial.println(l_dist);
+    //* DEBUG PASSED VARIABLES */
+//    Serial.print("task: ");
+//    Serial.println(task);
+//    Serial.print("rotation: ");
+//    Serial.println(rotation);
+//    Serial.print("rpm: ");
+//    Serial.println(rpm);
 
-    /* DEBUG PASSED VARIABLES */
-    Serial.print("task: ");
-    Serial.println(task);
-    Serial.print("rotation: ");
-    Serial.println(rotation);
-    Serial.print("rpm: ");
-    Serial.println(rpm);
 
     //* TEST PID */
 //     double val = MotorL.setRpm(40);
@@ -244,30 +273,45 @@ void loop() {
 //     Serial.println(MotorL.getRpm());
 //     Serial.print(" Output pwm: ");
 //     Serial.println(val);
+//     Robawt.setSteer(40, 0);   
 
-//     Robawt.setSteer(40, 0);
-     
-  } else {
-
-    // startChangeMillis = millis();
-
-    // claw_close();
-
+  } 
+  else {
     
+    // startChangeMillis = millis();
+    // claw_close();
     // Serial.println("0");
 
     //* TO MAKE ROBOT STOP */
-   Robawt.setSteer(0, 0);
-   Robawt.reset();
+    Robawt.setSteer(0, 0);
+    Robawt.reset();
 
     //* TO MAKE LEFT MOTOR STOP */
     // MotorL.setRpm(0);
     // MotorL.resetPID();
   }
 
+  //* DEBUG SWITCH STATE (1 is off, 0 is on)*/
   // Serial.print("Switch state");
   // Serial.println(digitalRead(SWTPIN));
+
+    //* DEBUG MOTOR DISTANCES */
+//  Serial.print("Distance (cm): ");
+//  Serial.println(MotorL.getDist());
+//  Serial.print("Angle (deg): ");
+//  Serial.println(MotorL.getAngle());
+//  Serial.print("Enc Val: ");
+//  Serial.println(MotorL.getEncVal());
+
+    //* READ LIDAR */
+//    tcaselect(4);
+//    front_dist = front_tof.readRangeContinuousMillimeters();
+//    Serial.println(front_dist);
 }
+
+//* -----------END LOOP-----------*//
+
+
 
 
 //* FUNCTIONS */
@@ -293,7 +337,7 @@ void serialEvent()
   }
 }
 
-void tcaselect(uint8_t i)
+void tcaselect(uint8_t i)  //^ DOM: this is the MUX/multiplexer
 {
   if (i > 0 && i < 7) {
     Wire.beginTransmission(TCAADDR);
--- pi/linetrack.py
+++ pi/linetrack.py
@@ -29,15 +29,15 @@ ser = serial.Serial("/dev/serial0", 9600)
 u_black = 70
 
 l_blue = np.array([96, 170, 80], np.uint8)
-u_blue = np.array([106, 245, 191], np.uint8)
+u_blue = np.array([106, 245, 191], np.uint8) #! blue needs more tuning to account for lighting 
 
 l_green = np.array([30, 50, 60], np.uint8)
 u_green = np.array([85, 255, 255], np.uint8)
 
-l_red1 = np.array([0, 100, 80], np.uint8) #! untuned red values
+l_red1 = np.array([0, 100, 80], np.uint8) 
 u_red1 = np.array([15, 255, 255], np.uint8)
 l_red2 = np.array([170, 100, 80], np.uint8) 
-u_red2 = np.array([180, 255, 255], np.uint8) #! 179 or 180?
+u_red2 = np.array([180, 255, 255], np.uint8)
 
 #* CONSTANTS CALIBRATION
 gs_roi_h = 300 #the crop height for gs #! increase after tuning  
@@ -46,18 +46,29 @@ gs_bksampleh = 40
 gs_minbkpct = 0.35 #! to be tuned properly #DOM: this is gs_minimum_black_percentage
 gs_minarea = 4000000 #^ consider making this scaled by pixels (/255)
 
-b_minarea = 10000 #! ~DOM: to tune the values 
+b_minarea = 1000000 #! ~DOM: to tune the values 
 
 rpm_setpt = 40
 rpm = 40
-kp = 1.5 #constant used for PID
+kp = 2 #constant used for PID
 
 #* IMAGE PROCESSING
 x_com = np.tile(np.linspace(-1., 1., width), (height, 1)) #reps is (outside, inside)
-y_com = np.array([[i] * width for i in np.linspace(1., 1/height, height)]) #1/height is just to save pixels
-x_com_scale = ((1-y_com) ** 0.6)
+# y_com = np.array([[i] * width for i in np.linspace(1., 1/height, height)]) #1/height is just to save pixels
+# x_com_scale = ((1-y_com) ** 0.6)
+y_com = np.array([[i] * width for i in np.linspace(1., 0, height)])
+x_com_scale = ((1-y_com) ** 0.3)
+x_gap_scale = y_com ** 0.4
+x_gap_com = x_com * x_gap_scale
 x_com *= x_com_scale
 #^ not scaled according to available picture!!
+
+gap_mask = np.zeros([height, width], dtype="uint8") #? DOM: Can we stick to one naming convention for mask variables, ie. mask_[object]
+peak_triangle_gap = 20
+points = np.array([[0, 0], [width, 0], [int(width/2), int(height-peak_triangle_gap)]])
+cv2.fillConvexPoly(gap_mask, points, 255)
+gap_x_com = cv2.bitwise_and(x_com, x_com, mask=gap_mask)  #? DOM: Also why's there a x_gap_com variable above and a gap_x_com var here :skull:
+gap_y_com = cv2.bitwise_and(y_com, y_com, mask=gap_mask)
 
 gs_erode_kernel = np.ones((3, 3), np.uint8)
 
@@ -76,16 +87,11 @@ class Task(enum.Enum):
 curr = Task.EMPTY
 red_now = False
 gs_now = False
-blue_now = False #^ not sure if this is necessary; might be quite superfluous
+gap_now = False
+blue_now = False 
 
 #~ Rescue kit
-reversing_now = False #for rescue kit
-moving_towards_blue = False #^ IM SORRY I KNOW THIS IS DISGUSTING :sob: lmao :eyes:
-journey = [] #to record the rotations of the robot
-
-#temporary
-start_blue = 0
-stop_blue = 0
+reversing_now = False #for rescue kit #^ DOM: i think i can remove this; hopefully
 
 #* BEGIN OF LINETRACK LOOP CODE
 
@@ -105,11 +111,11 @@ while True:
     mask_green = cv2.inRange(frame_hsv, l_green, u_green)
     mask_gs = mask_green[gs_roi_h:, :]
     # cv2.imshow("green square mask", frame_org[gs_roi_h:, :])
-    #cv2.imshow("green square mask", mask_gs) #& debug green square mask
+    # cv2.imshow("green square mask", mask_gs) #& debug green square mask
     gs_sum = np.sum(mask_gs)
-    # print(gs_sum) #& debug green min area
+    # print("Green sum:", gs_sum) #& debug green min area
 
-    mask_black = cv2.inRange(frame_gray, 0, u_black) - mask_green
+    mask_black_org = cv2.inRange(frame_gray, 0, u_black) - mask_green
     # cv2.imshow('black frame', black_mask) #& debug black mask
     # print(np.sum(mask_black), curr.name)
 
@@ -120,6 +126,7 @@ while True:
     # print(np.sum(mask_red)) #& debug red min area
 
     mask_blue = cv2.inRange(frame_hsv, l_blue, u_blue) 
+    mask_blue = mask_blue[100:, :]
     blue_sum = np.sum(mask_blue)
     #print(blue_sum) #& debug blue mask
 
@@ -137,9 +144,9 @@ while True:
     #TODO XEL make single green squares for longer
 
     #~ Turn while still seeing green
-    elif gs_now and gs_sum < 1000:
+    elif gs_now and gs_sum < 20000:
         if curr.name == "DOUBLE_GREEN":
-            if np.sum(mask_black) > 18000000:
+            if np.sum(mask_black) > 15000000:
                 gs_now = False
         else:
             gs_now = False
@@ -150,14 +157,14 @@ while True:
     elif not gs_now and gs_sum > gs_minarea:
         mask_gs = cv2.erode(mask_gs, gs_erode_kernel, iterations=1)
         mask_gs = cv2.dilate(mask_gs, gs_erode_kernel, iterations=1)
-        #cv2.imshow("after erosion and dilation", mask_gs) #& debug green square mask
+        # cv2.imshow("after erosion and dilation", mask_gs) #& debug green square mask
 
         #~ Find y position of green (Can cut processing time here by putting fixed constant instead)
         green_row = np.amax(mask_gs, axis=1)
         g_indices_v = np.where(green_row==255) #v for vertical
         gs_top = g_indices_v[0][0] + gs_roi_h
         gs_bot = g_indices_v[0][-1] + gs_roi_h
-        #cv2.imshow("Green bounding box", frame_org[gs_top:, gs_left:gs_right]) #& debug green bounds
+        # cv2.imshow("Green bounding box", frame_org[gs_top:, gs_left:gs_right]) #& debug green bounds
 
         if gs_bot > 470:
             #~ Find x positions of green
@@ -168,7 +175,7 @@ while True:
             # print("left:", gs_left, "right:", gs_right)
 
             #~ Test if below or above line
-            gs_bkabove = mask_black[gs_top - gs_bksampleoffset - gs_bksampleh : gs_top - gs_bksampleoffset, gs_left : gs_right]
+            gs_bkabove = mask_black_org[gs_top - gs_bksampleoffset - gs_bksampleh : gs_top - gs_bksampleoffset, gs_left : gs_right]
             gs_bkpct = np.sum(gs_bkabove) / 255 / gs_bksampleh / (gs_right - gs_left)
             # print("Percentage of black above green:", gs_bkpct) #& debug green's black
             # cv2.imshow("black area above green square", gs_bkabove) 
@@ -181,7 +188,7 @@ while True:
                 gs_centre = (gs_left + gs_right) / 2
 
                 #~ Find x position of black
-                gs_bkbeside = mask_black[gs_roi_h:, :]
+                gs_bkbeside = mask_black_org[gs_roi_h:, :]
                 blackM = cv2.moments(gs_bkbeside)
                 cx_black = int(blackM["m10"]/blackM["m00"]) if np.sum(gs_bkbeside) else 0 
                 #? theoretically divide by zero error should never happen 
@@ -202,79 +209,45 @@ while True:
                     gs_now = True
                 # else:
                     # print("ERROR: Green found but type indetermined")
-
-    #TODO: LOGIC HANDLING FOR WHEN + AFTER THE BOT PICKS UP THE RESCUE KIT?                                 
-
-    # #* REVERSING MOVEMENTS (RESCUE KIT)
-    # #^ DOM: thought process: reverse translation, then reverse rotation; handle movement on pico side
-    # #^ Instead of time.time(), use TOF sensors to record time taken for bot to move
-    # #^ XEL: the tof sensors dont read time btw you'll have to use millis() or elapsedMillis() on the pico
-    # if reversing_now: 
-    # #? XEL: ^ shouldnt this be elif because red takes precedence (line 122)
-    # #? DOM: ^ ok no but what if the rescue kit is directly adjacent to the red line and then it detects the red strip instead 
-    # #?        of completing the rescue kit task;; anyways it shld complete the rescue kit task before doing anything else right
-    # #? XEL: Fair point fair point
-    #     #~ If not enough time has elapsed since bot started moving backwards
-    #     if time.time() - stop_blue < (stop_blue - start_blue): 
-    #         curr = Task.BLUE #^ rpm will be negative on pico side
-    #         rpm = 40 
-    #     #~ If bot still has to rotate to its original rotation
-    #     elif len(journey) > 0:
-    #         curr = Task.EMPTY #^ A stopgap for now; more sophisticated way of doing this?
-    #         #print(len(journey)) #& debug rotations of bot
-    #         rotation = -(journey.pop())  #negative of the angle? adding 90 since original rotation ranges from 0 to 180 #XEL: removed the 90
-    #     else:
-    #         blue_now = False
-    #         reversing_now = False; 
-    #         curr = Task.EMPTY
+                                
 
     #* DETECTION OF RESCUE KIT
+    #^ Note from DOM: Will defo clean up the code once claw works haha yes
 
-    # #~ If close enough to rescue kit:
-    if not reversing_now and blue_sum >= 10000000: #^ DOM: I foresee some problems when bot tries to pick up block & still sees it? add additional condition of not reversing_now?
-        #time.sleep(2) #^ DOM: I assume the bot will pick up the cube here somehow?
-        # stop_blue = time.time()
-        # reversing_now = True
-        # moving_towards_blue = False #^ sorry 
-        # time_elapsed = stop_blue - start_blue
-        # dist_travelled = rpm * time_elapsed
+    #~ If close enough to rescue kit: (temporary, will use lidar to detect proximity instead)
+    if not reversing_now and blue_sum >= 14000000: 
         blue_now = True
-        curr = Task.BLUEFINAL
-        reversing_now = True
-
+        reversing_now = True 
+        curr = Task.BLUEFINAL  #^ DOM: please remove BLUEFINAL later thxxx
 
     #~ Rescue kit spotted:
-    #^ Current plan: handle movement of the robot on the pico side
     elif not reversing_now and blue_sum >= b_minarea:
-        print(blue_sum) #& debug blue sum
+        print("Blue sum: ", blue_sum) #& debug blue sum
         blue_now = True
-        curr = Task.BEFORE_BLUE
+        if (curr.name != "BLUE" and curr.name != "BLUEFINAL"):
+            curr = Task.BEFORE_BLUE
+
         #~ Find x and y positions of rescue kit
-        mask_blue = mask_blue[100:, :]
         blueM = cv2.moments(mask_blue)
-        # cv2.imshow("blue mask", mask_blue) #& debug blue mask
-        cx_blue = int(blueM["m10"] / blueM["m00"]) if np.sum(mask_blue) else 0 
-        cy_blue = int(blueM["m01"] / blueM["m00"]) if np.sum(mask_blue) else 0 
+        cv2.imshow("blue mask", mask_blue) #& debug blue mask
+        cx_blue = int(blueM["m10"] / blueM["m00"]) if np.sum(mask_blue) else 0 #^ nvm tentative: changed from "else 0" to "else cx_blue" 
+        cy_blue = int(blueM["m01"] / blueM["m00"]) if np.sum(mask_blue) else 0
         finalx = cx_blue - frame_org.shape[1]/2
-        finaly = frame_org.shape[0]/2 - cy_blue
+        #finaly = frame_org.shape[0]/2 - cy_blue
 
         #~ if the block isn't centred
-        if abs(cx_blue-finalx) >= 10: #^ DOM: not refined yet; consider doing mod(finalx) < 100
-            if cx_blue < frame_org.shape[1]/2: #if rescue kit is left of the middle of frame
-                rotation = -20 #^ Fixed rotation of the bot DOM: Ok apparently 20 degrees is kinda slow lol; multiply by kp?
+        if abs(finalx) >= 60: #^ DOM: not refined yet; consider doing abs(...) >= 100
+            if cx_blue < frame_org.shape[1]/2: #if rescue kit is to the left of the frame's middle
+                rotation = -20 #^ Fixed rotation of the bot; DOM: try 45 degrees instead? ie. only turn 1 wheel
             elif cx_blue > frame_org.shape[1]/2: #to the right
                 rotation = 20
 
+        #~ else if the block is (relatively) centred:
         else:
             curr = Task.BLUE
 
-            #! Comment out the bottom and uncomment the top later
-            # deviation = math.atan(finalx/finaly) * 180/math.pi if finaly != 0 else 0 #conversion of angle from bot to rescue kit into degrees 
-            # rotation = deviation * kp
-        # elif not moving_towards_blue: #^ if block is centred: start timer and start moving towards block (maybe track the distance travelled too?)
-        #     start_blue = time.time() #time when bot starts moving towards blue
-        #     moving_towards_blue = True #^ this is dumbbb but its to ensure that this code runs only once
-    elif blue_now: #^ Temporary
+    #~ No longer sees the rescue kit
+    elif blue_now and blue_sum < 2000: 
         blue_now = False
         reversing_now = False
 
@@ -283,24 +256,38 @@ while True:
     if not gs_now and not red_now and not blue_now:
 
         curr = Task.EMPTY
-        mask_gap = mask_black[crop_h_bw_gap:, :] 
-        mask_black = mask_black[crop_h_bw:, :]
-        # cv2.imshow("black lt mask", mask_gap)
-        #^ maybe try, not needed (og kernel=5,5)
-        # black_mask = cv2.erode(black_mask, kernel) 
-        # black_mask = cv2.dilate(black_mask, kernel)
+        # mask_gap = mask_black[crop_h_bw_gap:, :] 
+        mask_black = mask_black_org[crop_h_bw:, :]
+        black_kernel = np.ones((5, 5), np.uint8)
+        mask_black = cv2.erode(mask_black, kernel=black_kernel)
+        mask_black = cv2.dilate(mask_black, kernel=black_kernel)
+        # cv2.imshow("Black lt mask", mask_gap)
 
-        #~ Vectorizing the black components
-        y_black = cv2.bitwise_and(y_com, y_com, mask = mask_black)
-        x_black = cv2.bitwise_and(x_com, x_com, mask = mask_black)
-        # cv2.imshow("yframe", y_black)
-        # cv2.imshow("xframe", x_black)
+        #~ Find y positions of black
+        black_row = np.amax(mask_black, axis=1)
+        white_indices = np.where(black_row==0)
+        gap_start_y = white_indices[0][-1] if len(white_indices[0]) else 0
 
-        #~ Line gap
+        if gap_start_y < height-peak_triangle_gap: #if no gap
 
-        # print("max black:", np.max(y_black))
+            #~ Mask everything above the gap
+            mask_black = mask_black[gap_start_y:height, :]
+            # cv2.imshow("after gap accounted for", mask_black) #& debug black lt mask
+            y_black_com = y_com[gap_start_y:height, :]
+            x_black_com = x_com[gap_start_y:height, :]
 
-        #~ Plain line track
+            #~ Vectorizing the black components
+            y_black = cv2.bitwise_and(y_black_com, y_black_com, mask = mask_black)
+            x_black = cv2.bitwise_and(x_black_com, x_black_com, mask = mask_black)
+            # cv2.imshow("yframe", y_black)
+            # cv2.imshow("xframe", x_black)
+
+        else:
+            y_black = cv2.bitwise_and(gap_y_com, gap_y_com, mask = mask_black)
+            x_black = cv2.bitwise_and(gap_x_com, gap_x_com, mask = mask_black)
+            print("Gap")
+
+        #~ Line track values
         y_resultant = np.mean(y_black)
         x_resultant = np.mean(x_black)
 
@@ -312,9 +299,9 @@ while True:
     
     #* SEND DATA TO PICO
 
-    print("rpm:", rpm) #& debug sent variables
+    #print("rpm:", rpm) #& debug sent variables
     print("rotation:", rotation)
-    print("task:", curr.value)
+    #print("task:", curr.value)
 
     rotation = int(rotation)
     if rotation > 90:
@@ -323,8 +310,6 @@ while True:
         rotation = -90
     rotation += 90
 
-    # if blue_now: #^ consider removing this later
-    #     journey.append(rotation)
 
     to_pico = [255, rotation, # 0 to 180, with 0 actually being -90 and 180 being 90
                 254, rpm, # 0 to 200 MAX, but 100 really damn fast alr
