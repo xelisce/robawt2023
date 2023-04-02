95e6dd2c404fd6d08f16ef207341d3242976bc42
xelisce <xelisce@gmail.com>
2023-04-02 20:26:34 +0800

    updated hsvcalib to save to another folder: and pi

--- pi/hsvcalib1.py
+++ pi/hsvcalib1.py
@@ -5,6 +5,7 @@ import cv2
 
 cam_stream = WebcamStream(stream_id=0)
 cam_stream.start()
+i = 0
 
 
 while True:
@@ -12,11 +13,15 @@ while True:
         break
     else:
         frame = cam_stream.read()
+        frame = cv2.flip(frame, 0)
+        frame = cv2.flip(frame, 1)
 
     cv2.imshow('frame', frame)
     key = cv2.waitKey(1)
     if key == ord('k'): #press 'k' to take a photo 
-        cv2.imwrite("hsvcalibimage.jpg", frame)
+        cv2.imwrite(f"images/hsvcalibimage{i}.jpg", frame)
+        print(f"saved to images/hsvcalibimage{i}.jpg")
+        i += 1
     elif key == ord('q'): #press q to quit
         break
 
