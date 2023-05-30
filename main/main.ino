65b0f3a17bf18dfe1df2b27533a125c59974b737
xelisce <xelisce@gmail.com>
2023-05-30 09:03:45 +0800

    manz

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,98 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <TCA9548A.h>
+#include <Adafruit_TCS34725.h>
+#include <Vroom.h>
+
+Motor MotorR(13, 12, 1, 0); 
+Motor MotorL(10, 11, 18, 19);
+Vroom Robawt(&MotorL, &MotorR);
+
+Adafruit_TCS34725 tcs1 = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_2_4MS, TCS34725_GAIN_1X);
+Adafruit_TCS34725 tcs2 = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_2_4MS, TCS34725_GAIN_1X);
 
 TCA9548A mux(6, 7, &Wire1, 400000);
+
+uint16_t r1, g1, b1, c1, colorTemp1, lux1;
+uint16_t r2, g2, b2, c2, colorTemp2, lux2;
+
+uint16_t w_1 = 12, w_2 = 10;
+uint16_t b_1 = 1, b_2 = 1;
+double left, right, steer;
+long start_time, start_time_tcs1, end_time;
+
+void ISRLA() { MotorL.readEncA(); }
+void ISRLB() { MotorL.readEncB(); }
+void ISRRA() { MotorR.readEncA(); }
+void ISRRB() { MotorR.readEncB(); }
+
 
 void setup()
 {
     mux.begin();
+
     Serial.begin(9600);
     while (!Serial) delay(10);
     Serial.println("Done");
 
-    if (mux.select(2)) Serial.println("Done2");
+    mux.select(2);
+    if (!tcs1.attach(0x29, &Wire1)) {
+        Serial.println("ERROR: TCS34725 NOT FOUND !!!");
+        while (1);
+    } else {
+        Serial.println("Success!");
+    }
+
+    mux.select(4);
+    if (!tcs2.attach(0x29, &Wire1)) {
+        Serial.println("ERROR: TCS34725 NOT FOUND !!!");
+        while (1);
+    } else {
+        Serial.println("Success!");
+    }
+
+    pinMode(28, INPUT);
+    attachInterrupt(MotorL.getEncAPin(), ISRLA, RISING);
+    attachInterrupt(MotorL.getEncBPin(), ISRLB, RISING);
+    attachInterrupt(MotorR.getEncAPin(), ISRRA, RISING);
+    attachInterrupt(MotorR.getEncBPin(), ISRRB, RISING);
 }
 
-void loop() {}
+void loop() 
+{
+
+    mux.select(2);
+    start_time_tcs1 = millis();
+    tcs1.getRawData(&r1, &g1, &b1, &c1);
+    end_time = millis();
+    Serial.print("Time for 1 tcs: ");
+    Serial.println(end_time - start_time_tcs1);
+
+    // lux1 = tcs1.calculateLux(r1, g1, b1);
+    Serial.print(" | right: ");
+    // Serial.print(lux1);
+    lux1 = 0;
+    right = (double)(lux1 - b_1)/(w_1 - b_1);
+
+    mux.select(4);
+    tcs2.getRawData(&r2, &g2, &b2, &c2);
+    // lux2 = tcs2.calculateLux(r2, g2, b2);
+    Serial.print(" | left: ");
+    // Serial.print(lux2);
+    lux2 = 0;
+    left = (double)(lux2 - b_2)/(w_2 - b_2);
+
+    steer = (double)(left - right);
+    Serial.print(" | steer: ");
+    Serial.println(steer);
+
+    if (digitalRead(28)) {Robawt.setSteer(30, steer);}
+    else {Robawt.setSteer(0, 0);
+    Robawt.reset();}
+
+    // Serial.print("time: ");
+    // Serial.print(millis() - start_time);
+}
 
 
 // //* LIBRARIES
